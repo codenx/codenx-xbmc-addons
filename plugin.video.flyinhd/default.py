0a1899cd956359c332b5e5fbe67b4d364335738d
Code 'n X <senkrish@gmail.com>
2013-12-24 17:05:51 -0800

    Fixed youtube links in movie page.

--- plugin.video.flyinhd/default.py
+++ plugin.video.flyinhd/default.py
@@ -68,6 +68,225 @@ def parseMoviePage( lang ):
 
    return movieIndex
 
+def parseDailymotion( url ):
+   videos = []
+   link = urllib2.urlparse.urlsplit( url )
+   netloc = link.netloc
+   path = link.path
+
+   print "dailymotion url : " + url
+
+   def parseDailymotionPlaylist( playlistId ):
+      videos = []
+      dlurl = 'https://api.dailymotion.com/playlist/' + playlistId + '/videos'
+      try:
+         response = net.http_GET( dlurl )
+         html = response.content
+      except urllib2.HTTPError, e:
+         print "HTTPError : " + str( e )
+         return videos
+
+      jsonObj = json.loads( html )
+      for video in jsonObj['list']:
+         videos.append( 'http://www.dailmotion.com/video/' + str(video['id']) )
+      return videos
+         
+   # Handle playlists
+   playlistId = ''
+   if 'jukebox' in url:
+      playlistId = re.compile("\?list[\]\=\%2Fplaylist\%2F(.+?)&").findall( url )[ 0 ]
+   elif 'playlist' in url:
+      playlistId = re.compile("playlist/(.+?)_").findall( url )[ 0 ]
+   elif 'video/' in url:
+      videoId = re.compile("video/(.+)").findall( path )[ 0 ]
+      videoId = videoId.split( '_' )[ 0 ]
+   elif 'swf' in url:
+      videoId = re.compile("swf/(.+)").findall( path )[ 0 ]
+   else:
+      print "unknown dailymotion link"
+      return videos
+
+   if playlistId:
+      print "playlistId : " + playlistId
+      videos += parseDailymotionPlaylist( playlistId )
+   else:
+      videos += [ 'http://' + netloc + '/' + videoId ]
+   return videos
+      
+def parseYoutube( url ):
+   videos = []
+   link = urllib2.urlparse.urlsplit( url )
+   query = link.query
+   netloc = link.netloc
+   path = link.path
+
+   print "youtube url : " + url
+
+   def parseYoutubePlaylist( playlistId ):
+      videos = []
+      yturl = 'http://gdata.youtube.com/feeds/api/playlists/' + playlistId
+      try:
+         response = net.http_GET( yturl )
+         html = response.content
+      except urllib2.HTTPError, e:
+         print "HTTPError : " + str( e )
+         return videos
+
+      soup = BeautifulSoup( html )
+
+      for video in soup.findChildren( 'media:player' ):
+         videoUrl = str( video[ 'url' ] )
+         print "youtube video : " + videoUrl
+         videos += parseYoutube( videoUrl )
+      return videos
+
+   # Find v=xxx in query if present
+   qv = ''
+   if query:
+      qs = cgi.parse_qs( query )
+      qv = qs.get( 'v', [''] )[ 0 ]
+      if qv:
+         qv = '?v=' + qv
+   
+   # Handle youtube gdata links
+   playlistId = ''
+   if re.search( '\?list=PL', url ):
+      playlistId = re.compile("\?list=PL(.+?)&").findall( url )[ 0 ]
+   elif re.search( '\?list=', url ):
+      playlistId = re.compile("\?list=(.+?)&").findall( url )[ 0 ]
+   elif re.search( '/p/', url ):
+      playlistId = re.compile("/p/(.+?)(?:/|\?|&)").findall( url )[ 0 ]
+   elif re.search( 'view_play_list', url ):
+      plyalistId = re.compile("view_play_list\?.*?&amp;p=(.+?)&").findall( url)[ 0 ]
+
+   if playlistId:
+      print "playlistId : " + playlistId
+      videos += parseYoutubePlaylist( playlistId )
+   else:
+      videos += [ 'http://' + netloc + path + qv ]
+   return videos
+
+def Load_Video( name, url ):
+   print "load_video:" + name + ":" + url
+   response = net.http_GET( url )
+   net.save_cookies( cookieFile )
+   html = response.content
+   stream_url = re.compile( "file: \"(.+?)\"" ).findall( html )
+   if stream_url:
+      stream_url = stream_url[ 0 ]
+      print "url:" + stream_url
+
+      pDialog = xbmcgui.DialogProgress()
+      pDialog.create('Streaming ' + name )
+
+      playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+      playlist.clear()
+      listitem = xbmcgui.ListItem( name )
+      playlist.add(stream_url, listitem)
+      xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(playlist)
+      return
+
+   soup = BeautifulSoup( html )
+   sourceVideos = []
+
+   # Handle href tags
+   for a in soup.findAll('a', href=True):
+      if a['href'].find("youtu.be") != -1:
+         sourceVideos.append('src="' + (a['href'].split()[0]) + '" ')
+         
+      if a['href'].find("youtube") != -1:
+         sourceVideos.append('src="' + (a['href'].split()[0]) + '" ')
+         
+      if a['href'].find("dailymotion") != -1:
+         sourceVideos.append('src="' + (a['href'].split()[0]) + ' ' +  ('width = ""'))
+
+   # Handle embed tags
+   #sourceVideos += re.compile( '<embed(.+?)>', flags=re.DOTALL).findall( html )
+
+   # Handle iframe tags
+   sourceVideos += re.compile( '<iframe(.+?)>').findall( html )
+
+   # Handle Youtube new window
+   src = re.compile( 'onclick="window.open\((.+?),' ).findall( html )
+   if src:
+      sourceVideos += [ 'src=' + src[ 0 ] ]
+
+   if len( sourceVideos ) == 0:
+      print "No video sources found!!!!"
+      addon.show_ok_dialog( [ 'Page has unsupported video' ], title='Playback' )
+      return
+      
+   videoItem = []
+   for sourceVideo in sourceVideos:
+      print "sourceVideo=" + sourceVideo
+      sourceVideo = re.compile( 'src=(?:\"|\')(.+?)(?:\"|\')' ).findall( sourceVideo )[0]
+      sourceVideo = urllib.unquote( sourceVideo )
+      print "sourceVideo=" + sourceVideo
+      link = urllib2.urlparse.urlsplit( sourceVideo )
+      host = link.hostname
+      host = host.replace( 'www.', '' )
+      host = host.replace( '.com', '' )
+      sourceName = host.capitalize()
+      print "sourceName = " + sourceName
+
+      if 'dailymotion' in host:
+         sourceVideo = parseDailymotion( sourceVideo )
+         for video in sourceVideo:
+            print "sourceVideo : " + video
+            videoId = re.compile('dailymotion\.com/(.+)').findall( video )[ 0 ]
+            video = 'plugin://plugin.video.dailymotion_com/?mode=playVideo&url=' + videoId
+            videoItem.append( (video, sourceName, video ) )
+
+      elif 'youtube' in host:
+         sourceVideo = parseYoutube( sourceVideo )
+         for video in sourceVideo:
+            print "sourceVideo : " + video
+            hosted_media = urlresolver.HostedMediaFile( url=video, title=sourceName )
+            if not hosted_media:
+               print "Skipping video " + sourceName
+               continue
+            videoItem.append( (video, sourceName, hosted_media ) )
+
+      else:
+         print "sourceVideo : " + sourceVideo
+         hosted_media = urlresolver.HostedMediaFile( url=sourceVideo, title=sourceName )
+         if not hosted_media:
+            print "Skipping video " + sourceName
+            continue
+         videoItem.append( (sourceVideo, sourceName, hosted_media ) )
+
+   if len( videoItem ) == 0:
+      addon.show_ok_dialog( [ 'Video does not exist' ], title='Playback' )
+   elif len(videoItem) == 1:
+      url, title, hosted_media = videoItem[ 0 ]
+      if 'dailymotion' in url:
+         stream_url = url
+      else:
+         stream_url = hosted_media.resolve()
+      print "stream_url " + stream_url
+
+      pDialog = xbmcgui.DialogProgress()
+      pDialog.create('Opening stream ' + title)
+
+      playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+      playlist.clear()
+      listitem = xbmcgui.ListItem(title)
+      playlist.add(stream_url, listitem)
+      xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(playlist)
+   else:
+      partNo = 1
+      prevSource = ''
+      for sourceVideo, sourceName, _ in videoItem:
+         if sourceName != prevSource:
+            partNo = 1
+            prevSource = sourceName
+
+         title = sourceName + ' Part# ' + str( partNo )
+         addon.add_video_item( { 'url' : sourceVideo }, { 'title' : title } )
+         partNo += 1
+
+      xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
 def Main_Categories():
    print "cookie path:" + cookiePath
    print "cookie file:" + cookieFile
@@ -164,23 +383,6 @@ def Movie_Recent( url, lang ):
                            img=img, total_items=len(div) )
 
    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def Load_Video( name, url ):
-   print "load_video:" + name + ":" + url
-   response = net.http_GET( url )
-   net.save_cookies( cookieFile )
-   html = response.content
-   stream_url = re.compile( "file: \"(.+?)\"" ).findall( html )[ 0 ]
-   print "url:" + stream_url
-
-   pDialog = xbmcgui.DialogProgress()
-   pDialog.create('Streaming ' + name )
-
-   playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-   playlist.clear()
-   listitem = xbmcgui.ListItem( name )
-   playlist.add(stream_url, listitem)
-   xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(playlist)
 
 ##### Queries ##########
 mode = addon.queries['mode']
